4a259a15114f7a3683fc68c1e0c06674d6b2e4cd
cooke09 <pozdeev199@mail.ru>
2023-02-09 23:14:15 +0300

    Функцию поиска размера дерева убрал в staticmethod, обновлен type hinting

--- DataStructures/python_tree.py
+++ DataStructures/python_tree.py
@@ -4,18 +4,19 @@ class Node:
         self.left = self.right = None
 
 
-def get_size_of_tree(root: Node):
-    if not root:
-        return 0
-    return max(get_size_of_tree(root.right), get_size_of_tree(root.right)) + 1
-
-
 class Tree:
     def __init__(self, items: list[int] = None):
         self.root = None
         if not self.root and items:
             self.root = Node(items.pop(0))
         [self.add_item(item) for item in items]
+
+    @staticmethod
+    def get_size_of_tree(root: Node) -> int:
+        if not root:
+            return 0
+        return max(Tree.get_size_of_tree(root.right),
+                   Tree.get_size_of_tree(root.right)) + 1
 
     def __repr__(self):
         res = []
@@ -30,7 +31,7 @@ class Tree:
         res.append('None')
         return '->'.join(res)
 
-    def add_item(self, value):
+    def add_item(self, value: int):
         def inner(root: Node, data):
             if data < root.value:
                 if root.left:
@@ -63,7 +64,7 @@ class Tree:
         return root.value
 
     @property
-    def min_value(self):
+    def min_value(self) -> int:
         root = self.root
         while root and root.left:
             root = root.left
@@ -71,7 +72,7 @@ class Tree:
 
     def find_item(self, value: int) -> bool:
 
-        def inner(root, item):
+        def inner(root: Node, item: int) -> int | None:
             if root:
                 if root.value == item:
                     return root.value
@@ -82,8 +83,8 @@ class Tree:
 
         return True if inner(self.root, value) else False
 
-    def is_mirror(self):
-        def check_half(left: Node, right: Node):
+    def is_mirror(self) -> bool:
+        def check_half(left: Node, right: Node) -> bool:
             if not left or not right:
                 return False
             if left.value == right.value:
@@ -93,15 +94,15 @@ class Tree:
 
         return check_half(self.root.left, self.root.right)
 
-    def get_size(self):
-        return get_size_of_tree(self.root)
+    def get_size(self) -> int:
+        return self.get_size_of_tree(self.root)
 
-    def is_balanced(self):
-        def inner(root: Node):
+    def is_balanced(self) -> bool:
+        def inner(root: Node) -> bool:
             if not root:
                 return True
-            left_part = get_size_of_tree(root.left)
-            right_part = get_size_of_tree(root.right)
+            left_part = self.get_size_of_tree(root.left)
+            right_part = self.get_size_of_tree(root.right)
             difference_between = abs(left_part - right_part) <= 0
             if difference_between and inner(root.left) and inner(root.right):
                 return True
