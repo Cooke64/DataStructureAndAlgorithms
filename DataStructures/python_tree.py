19b092b2318dc51d07943ff144a01cf1c3886b34
cooke09 <pozdeev199@mail.ru>
2023-02-10 13:18:48 +0300

    Поиск по дереву, сумма всех элементов, сумма элементов в диапазоне.

--- DataStructures/python_tree.py
+++ DataStructures/python_tree.py
@@ -9,7 +9,7 @@ class Tree:
         self.root = None
         if not self.root and items:
             self.root = Node(items.pop(0))
-        [self.add_item(item) for item in items]
+        [self._add_node(self.root, item) for item in items]
 
     @staticmethod
     def get_size_of_tree(root: Node) -> int:
@@ -17,6 +17,16 @@ class Tree:
             return 0
         return max(Tree.get_size_of_tree(root.right),
                    Tree.get_size_of_tree(root.right)) + 1
+
+    @staticmethod
+    def find_item(root: Node, item: int) -> Node | None:
+        if root:
+            if root.value == item:
+                return root
+            if item < root.value and root.left:
+                return Tree.find_item(root.left, item)
+            elif item > root.value and root.right:
+                return Tree.find_item(root.right, item)
 
     def __repr__(self):
         res = []
@@ -31,20 +41,23 @@ class Tree:
         res.append('None')
         return '->'.join(res)
 
-    def add_item(self, value: int):
-        def inner(root: Node, data):
-            if data < root.value:
+    def _add_node(self, root: Node, value):
+        if root:
+            if value < root.value:
                 if root.left:
-                    inner(root.left, data)
+                    self._add_node(root.left, value)
                 else:
-                    root.left = Node(data)
+                    root.left = Node(value)
             else:
                 if root.right:
-                    inner(root.right, data)
+                    self._add_node(root.right, value)
                 else:
-                    root.right = Node(data)
+                    root.right = Node(value)
 
-        inner(self.root, value)
+    def add_item(self, value):
+        if not self.root:
+            self.root = Node(value)
+        self._add_node(self.root, value)
 
     def printer(self):
         def inner(root: Node):
@@ -70,21 +83,16 @@ class Tree:
             root = root.left
         return root.value
 
-    def find_item(self, value: int) -> bool:
-
-        def inner(root: Node, item: int) -> int | None:
-            if root:
-                if root.value == item:
-                    return root.value
-                if item < root.value and root.left:
-                    return inner(root.left, item)
-                elif item > root.value and root.right:
-                    return inner(root.right, item)
-
-        return True if inner(self.root, value) else False
+    def is_in_tree(self, value: int) -> bool:
+        return bool(self.find_item(self.root, value))
 
     def is_mirror(self) -> bool:
+        """
+        Дерево называется анаграммой, если оно симметрично относительно своего центра.
+        """
         def check_half(left: Node, right: Node) -> bool:
+            if not left and not left:
+                return True
             if not left or not right:
                 return False
             if left.value == right.value:
@@ -98,18 +106,52 @@ class Tree:
         return self.get_size_of_tree(self.root)
 
     def is_balanced(self) -> bool:
+        """
+        Дерево считается сбалансированным, если левое и правое
+        поддеревья каждой вершины отличаются по высоте не больше, чем на единицу.
+        """
         def inner(root: Node) -> bool:
             if not root:
                 return True
             left_part = self.get_size_of_tree(root.left)
             right_part = self.get_size_of_tree(root.right)
-            difference_between = abs(left_part - right_part) <= 0
+            difference_between = abs(left_part - right_part) <= 1
             if difference_between and inner(root.left) and inner(root.right):
                 return True
             return False
 
         return inner(self.root)
 
+    def add_before(self, value):
+        new_node = Node(value)
+        if not self.root:
+            self.root = new_node
+        if value < self.root.value:
+            self.root, new_node.right = new_node, self.root
+        else:
+            self.root, new_node.left = new_node, self.root
 
-tree = Tree([2, 4, 78, 345])
-print(tree.get_size())
+    def sum_tree(self):
+        def inner(root: Node):
+            if not root:
+                return 0
+            if root:
+                return root.value + inner(root.right) + inner(root.left)
+
+        return inner(self.root)
+
+    def sum_range(self, low, hight):
+
+        def inner(root: Node):
+            if not root:
+                return 0
+
+            check_node = low <= root.value <= hight
+            result = root.value if check_node else 0
+            return inner(root.left) + inner(root.right) + result
+
+        return inner(self.root)
+
+
+tree = Tree([1, 2, 3])
+print(tree.sum_tree())
